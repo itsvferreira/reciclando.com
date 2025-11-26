25c61dc4abaacf60b506a5d35cace6c31491549e
veronica95s <veronica.wq83@gmail.com>
2025-11-26 14:12:24 -0300

    'ServiceRecycler'

--- app/src/main/java/com/reciclando/app/repositories/RecyclerRepository.java
+++ app/src/main/java/com/reciclando/app/repositories/RecyclerRepository.java
@@ -1,8 +1,12 @@
 package com.reciclando.app.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.reciclando.app.Models.Recycler;
 
 public interface RecyclerRepository extends CrudRepository<Recycler, Long> {
+    List<Recycler> findByAcceptedMaterialsContaining(String material);
+    
 }
