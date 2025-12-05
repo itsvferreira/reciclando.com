2b93fb4d03a930e323cbed4d72d166bce8ac105d
veronica95s <veronica.wq83@gmail.com>
2025-12-05 11:16:25 -0300

    'conflitos'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HowToRecycle from "./pages/HowToRecycle";
 import Login from "./pages/Login";
+// import Ads from "./pages/Ads";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/how-to-recycle" element={<HowToRecycle />} />
+        <Route path="/anuncios" element={<Ads />}> </Route>
       </Routes>
     </Router>
   );
