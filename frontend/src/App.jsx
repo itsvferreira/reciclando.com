5db98b7266cc3166165500fcc1786214485ac780
veronica95s <veronica.wq83@gmail.com>
2025-12-08 10:32:55 -0300

    'conflito'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,27 @@
-import Header from "./components/Header"; 
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HowToRecycle from "./pages/HowToRecycle/HowToRecycle";
+import Login from "./pages/Login/Login";
+import Ads from "./pages/Ads/Ads";
+import UserProfile from "./pages/UserProfile/UserProfile";
+import RecyclerProfile from "./pages/RecyclerProfile/RecyclerProfile";
+import Recyclers from "./pages/Recyclers/Recyclers";
 import Home from "./pages/Home";
-import Footer from "./components/Footer";
-
 
 function App() {
   return (
-    <main>
-      <Header /> 
-      <Home />
-      <Footer />
-    
-    </main>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/how-to-recycle" element={<HowToRecycle />} />
+        <Route path="/anuncios" element={<Ads />} />
+        <Route path="/Ads" element={<Ads />} />
+        <Route path="/user-profile" element={<UserProfile />} />
+        <Route path="/recycler-profile" element={<RecyclerProfile />} />
+        <Route path="/recyclers" element={<Recyclers />} />
+      </Routes>
+    </Router>
   );
 }
 
